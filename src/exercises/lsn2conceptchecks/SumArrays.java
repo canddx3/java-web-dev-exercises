c70ee9de4700c72982a45ab479e8d2f4966bff75
chad <canddx3@gmail.com>
2021-02-05 12:24:55 -0600

    First commit

--- src/exercises/lsn2conceptchecks/SumArrays.java
+++ src/exercises/lsn2conceptchecks/SumArrays.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package exercises.lsn2conceptchecks;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,24 +18,4 @@ public class SumArrays {
         return sum;
     }
 }
-=======
-package exercises.lsn2conceptchecks;
-import java.util.ArrayList;
-import java.util.Arrays;
 
-public class SumArrays {
-    public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-        System.out.println(sumEven(numbers));
-    }
-        public static int sumEven(ArrayList<Integer> arr) {
-        int sum = 0;
-        for(int d: arr) {
-            if (d % 2 == 0) {
-                sum += d;
-            }
-        }
-        return sum;
-    }
-}
->>>>>>> origin/master
