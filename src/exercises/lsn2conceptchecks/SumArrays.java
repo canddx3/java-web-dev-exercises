69fc1a01e08857000141727553890301b917e61d
Chad Conley <51886951+canddx3@users.noreply.github.com>
2021-02-05 11:35:17 -0600

    Add files via upload

---
+++ src/exercises/lsn2conceptchecks/SumArrays.java
@@ -0,0 +1,19 @@
+package exercises.lsn2conceptchecks;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SumArrays {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        System.out.println(sumEven(numbers));
+    }
+        public static int sumEven(ArrayList<Integer> arr) {
+        int sum = 0;
+        for(int d: arr) {
+            if (d % 2 == 0) {
+                sum += d;
+            }
+        }
+        return sum;
+    }
+}
